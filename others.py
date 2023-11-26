ec6dcb5dac32af9a10ceb9983a35a69bdd91a694
Vinícius Maximiliano <vinnemaax@gmail.com>
2023-11-26 01:46:54 -0300

    included all scrapping phase

---
+++ others.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.by import By
+import time
+
+url = f'https://querobolsa.com.br/unip/avaliacao-dos-alunos'
+
+options = webdriver.ChromeOptions()
+#options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(options=options) 
+driver.get(url)
+
+full_data = []
+
+for z in range(9):
+    author = driver.find_elements(By.XPATH, '//strong[@class="z-title student-evaluation__profile-name z-title--size-extra-small"]')
+    rev = driver.find_elements(By.XPATH, '//p[@class="z-text student-evaluation__text z-text--size-medium z-text--left"]')
+
+    reviews = lista_de_listas = [list(par) for par in zip(rev[::2], rev[1::2])]
+
+    for i in range(len(reviews)):
+        full_data.append([author[i].text, reviews[i][0].text, reviews[i][1].text])
+        #print(f'\nNome: {author[i].text}\npros: {reviews[i][0].text}\n\ncons: {reviews[i][1].text}\n===== <> ===== <> ===== <>')
+
+    next_button = driver.find_elements(By.XPATH, '//button[@class="paginator__button paginator__arrow-button"]')
+    next_button[1].click()
+    time.sleep(3)
+
+df = pd.DataFrame(full_data, columns=['author', 'pros', 'cons'])
+df.head(10)
+df.to_csv('./data/others/querobolsa.csv')
+
+print(df)
--- tweets.py
+++ tweets.py
@@ -1,9 +1,14 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
+from keys import *
 
-def obter_tweets(termo_pesquisa, num_tweets=10):
-    url = f'https://twitter.com/search?q={termo_pesquisa}&src=typed_query&f=live'
+def obter_tweets(num_tweets=50):
+    url = f'https://twitter.com/login'
 
     # Configurar o navegador
     options = webdriver.ChromeOptions()
@@ -12,25 +17,84 @@ def obter_tweets(termo_pesquisa, num_tweets=10):
     driver = webdriver.Chrome(options=options)  # Você precisa ter o ChromeDriver instalado e no PATH
     driver.get(url)
 
-    # Rolar a página para baixo para carregar mais tweets
+    time.sleep(3)
+    elemento_input = driver.find_element(By.XPATH,"//input[@name='text']")
+    elemento_input.send_keys(TWITTER_LOGIN)
+
+    next_button = driver.find_element(By.XPATH,"//span[contains(text(),'Avançar')]")
+    next_button.click()
+
+    time.sleep(3)
+
+    password = driver.find_element(By.XPATH,"//input[@name='password']")
+    password.send_keys(TWITTER_PASS)
+
+    log_in = driver.find_element(By.XPATH,"//span[contains(text(),'Entrar')]")
+    log_in.click()
+
+    time.sleep(2)
+
+    url_top = 'https://twitter.com/search?q=lang%3Apt%20Unip&src=typed_query&f=top'
+    driver.get(url_top)
+    time.sleep(2)
+
+    top_tweets_list = []
+    latest_tweets_list = []
+
+    for _ in range(num_tweets // 10):
+        
+        UserTag = driver.find_element(By.XPATH, ".//div[@data-testid='User-Name']").text
+        TimeStamp = driver.find_element(By.XPATH, ".//time").get_attribute('datetime')
+        Tweet = driver.find_element(By.XPATH, ".//div[@data-testid='tweetText']").text
+
+        top_tweets_list.append([UserTag, Tweet, TimeStamp])
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+
+    # Obter o conteúdo da página
+    page_content_top = driver.page_source
+
+    url_latest = 'https://twitter.com/search?q=lang%3Apt%20Unip&src=typed_query&f=live'
+    driver.get(url_latest)
+    time.sleep(2)
+
     for _ in range(num_tweets // 10):  # 10 tweets são carregados a cada rolagem
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)  # Aguardar um breve período após cada rolagem
 
-    # Obter o conteúdo da página
-    page_content = driver.page_source
-    driver.quit()
+    #page_content_latest = driver.page_source
+    #driver.quit()
 
     # Analisar a página com BeautifulSoup
-    soup = BeautifulSoup(page_content, 'html.parser')
-    tweets = soup.find_all('div', {'data-testid': 'tweet'})
+    top_page = BeautifulSoup(page_content_top, 'html.parser')
+    latest_page = BeautifulSoup(page_content_latest, 'html.parser')
 
-    for tweet in tweets:
+    top_tweets = top_page.find_all("//article[@data-testid='tweet']")
+
+    latest_tweets = latest_page.find_all("//article[@data-testid='tweet']")
+
+    for tweet in top_tweets:
         usuario = tweet.find('span', {'class': 'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'}).text
         conteudo = tweet.find('div', {'class': 'css-901oao r-1re7ezh r-1qd0xha r-n6v787 r-16dba41 r-1sf4r6n r-bcqeeo r-qvutc0'}).text
         data = tweet.find('time')['datetime']
 
-        print(f'Usuário: {usuario}\nConteúdo: {conteudo}\nData: {data}\n{"="*50}')
+        top_tweets_list.append([usuario, conteudo, data])
+
+    for tweet in latest_tweets:
+        usuario = tweet.find('span', {'class': 'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'}).text
+        conteudo = tweet.find('div', {'class': 'css-901oao r-1re7ezh r-1qd0xha r-n6v787 r-16dba41 r-1sf4r6n r-bcqeeo r-qvutc0'}).text
+        data = tweet.find('time')['datetime']
+
+        latest_tweets_list.append([usuario, conteudo, data])
+
+    df = pd.DataFrame(top_tweets_list, columns=['author', 'text', 'data'])
+    df.head(10)
+    df.to_csv('./data/twitter/top_tweets.csv')
+
+    df = pd.DataFrame(latest_tweets_list, columns=['author', 'text', 'data'])
+    df.head(10)
+    df.to_csv('./data/twitter/latest_tweets.csv')
 
 # Substituir 'python' pelo termo de pesquisa desejado
-obter_tweets('python', num_tweets=10)
+obter_tweets(num_tweets=50)
